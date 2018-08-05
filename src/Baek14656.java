ef68ce8be4c354435af232c4683381b5d9f97ca9
장용범 <dnwkqk@gmail.com>
2018-08-05 20:46:52 +0900

    solve baek14656

---
+++ src/Baek14656.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Baek14656 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] line = new int[n];
+        int count = 0;
+
+        for(int i=0;i<n;i++) line[i] = sc.nextInt();
+
+        for(int i=0;i<n;i++){
+            if(line[i] != i+1) count++;
+        }
+
+        System.out.println(count);
+
+    }
+}
