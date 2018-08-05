d3c954c877ba88ad8a0bfd7cab45060c32734a04
장용범 <dnwkqk@gmail.com>
2018-08-05 21:53:24 +0900

    solve baek1085

---
+++ src/Baek1085.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// 직사각형에서 탈출
+public class Baek1085 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        int min = x;
+
+        if(min > w-x) min = w-x;
+        if(min > y) min = y;
+        if(min > h-y) min = h-y;
+
+        System.out.println(min);
+    }
+}
