2e72f3ee800fbcee0e36559dc2281433177d9848
장용범 <dnwkqk@gmail.com>
2018-08-05 21:34:49 +0900

    solve baek2908

---
+++ src/Baek2908.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// 상수
+public class Baek2908 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        String aa = String.valueOf(a.charAt(2))+String.valueOf(a.charAt(1))+String.valueOf(a.charAt(0));
+        String bb = String.valueOf(b.charAt(2))+String.valueOf(b.charAt(1))+String.valueOf(b.charAt(0));
+
+        if(Integer.parseInt(aa) > Integer.parseInt(bb)) System.out.print(aa);
+        else System.out.print(bb);
+
+
+
+    }
+}
