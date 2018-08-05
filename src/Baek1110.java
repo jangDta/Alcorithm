94254af7ec1593d9fdd79827a941659e5082d23e
장용범 <dnwkqk@gmail.com>
2018-08-05 21:46:03 +0900

    solve baek1110

---
+++ src/Baek1110.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// 더하기 사이클
+public class Baek1110 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+        int origin = n;
+
+        do{
+            int a = n/10;
+            int b = n%10;
+
+            int c = a+b;
+
+            n = b*10+c%10;
+
+            //System.out.println("n:"+n);
+            count++;
+
+
+        }while(origin!=n);
+
+
+        System.out.print(count);
+
+
+    }
+}
