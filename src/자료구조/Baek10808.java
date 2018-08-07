7a35425735d532ba7282fa543e1a321236870843
장용범 <dnwkqk@gmail.com>
2018-08-07 19:32:30 +0900

    solve baek10808

---
+++ src/자료구조/Baek10808.java
@@ -0,0 +1,22 @@
+package 자료구조;
+
+import java.util.Scanner;
+
+// 알파벳 개수
+public class Baek10808 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        char[] chars = s.toCharArray();
+        int[] arr = new int[26];
+
+        for(int i=0;i<chars.length;i++){
+            int asci = (int)chars[i] - 97;
+            arr[asci]++;
+        }
+
+        for(int i=0;i<arr.length;i++) System.out.print(arr[i]+" ");
+
+    }
+}
