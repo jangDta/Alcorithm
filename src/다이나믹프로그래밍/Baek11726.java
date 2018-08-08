03afde3040b7c60a3cc03120cd7d058dde0c0f98
장용범 <dnwkqk@gmail.com>
2018-08-08 17:02:12 +0900

    solve baek11726

---
+++ src/다이나믹프로그래밍/Baek11726.java
@@ -0,0 +1,23 @@
+package 다이나믹프로그래밍;
+
+import java.util.Scanner;
+
+// 2xn 타일링
+public class Baek11726 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] %= 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
