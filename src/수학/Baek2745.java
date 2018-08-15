85dc4321e70f6f209364ec3d17e16580751f1a55
장용범 <dnwkqk@gmail.com>
2018-08-15 21:29:27 +0900

    solve baek2745

--- src/수학/Baek2745.java
+++ src/수학/Baek2745.java
@@ -16,30 +16,15 @@ public class Baek2745 {
 
             if(c >= 'A' && c <= 'Z'){ // A - Z 진법
                 int a = (int)(c-'A'+10);
-                res += Math.pow(b,n.length()-i-1);
+                res += (long)(a*Math.pow(b,n.length()-i-1));
             }else{
                 int a = (int)c - 48;
-                res += Math.pow(b,n.length()-i-1);
+                res += (long)(a*Math.pow(b,n.length()-i-1));
             }
         }
 
-//        System.out.println("res : "+res);
-//
-//        System.out.println(Math.pow(35,0)+Math.pow(35,1)+Math.pow(35,2)+Math.pow(35,3)+Math.pow(35,4));
-        ss();
-
+        System.out.println(res);
 
     }
 
-    static void ss(){
-        long r = 0;
-        for(int i=0;i<5;i++){
-            long a = 35*(long)Math.pow(36,i);
-            System.out.println("a:"+a);
-            r += a;
-
-        }
-
-        System.out.println("r:"+r);
-    }
 }
